c8a6a3bd7f6af3fc30710b7a42119308bb77e960
AlStien <aj601604@gmail.com>
2022-03-05 01:11:43 +0530

    customised admin for wallpapers

--- addiction/admin.py
+++ addiction/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from . import models
 
+class WallpaperAdmin(admin.ModelAdmin):
+    list_display = ('day', 'id')
+
 # Register your models here.
 admin.site.register(models.User)
-admin.site.register(models.Wallpaper)
+admin.site.register(models.Wallpaper, WallpaperAdmin)
