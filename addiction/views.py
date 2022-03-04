cb6040aeab94d72f160d8804afaf44bb7089133f
AlStien <aj601604@gmail.com>
2022-03-05 01:59:12 +0530

    modified user view for existing users

--- addiction/views.py
+++ addiction/views.py
@@ -1,18 +1,25 @@
+from rest_framework.response import Response
 from rest_framework import generics
 from . import serializers
 from .models import User, Wallpaper
+from .serializers import UserSerializer, WallpaperSerializer
 
 # Create your views here.
 class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = serializers.UserSerializer
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        if User.objects.filter(username=request.data['username']).exists():
+            return Response(UserSerializer(instance=User.objects.get(username = request.data['username'])).data)
+        return super().post(request, *args, **kwargs)
 
 class HomeView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
 class WallpaperGet(generics.ListAPIView):
-    serializer_class = serializers.WallpaperSerializer
+    serializer_class = WallpaperSerializer
 
     def get_queryset(self):
         return Wallpaper.objects.filter(day__lte = self.kwargs['pk'])
