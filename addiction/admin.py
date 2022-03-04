abd324a4f73ddcc23e299f703512134d9738a3a2
AlStien <aj601604@gmail.com>
2022-03-05 01:12:41 +0530

    customised admin for users

--- addiction/admin.py
+++ addiction/admin.py
@@ -4,6 +4,9 @@ from . import models
 class WallpaperAdmin(admin.ModelAdmin):
     list_display = ('day', 'id')
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'id')
+
 # Register your models here.
-admin.site.register(models.User)
+admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Wallpaper, WallpaperAdmin)
